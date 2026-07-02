7f37c981a2fed5d5b6d83ae83244d24ea0b09c7a
e <e>
2026-07-02 16:09:43 +0300

    Initial Commit

---
+++ Shader-in-blago/Shader_in_blagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shader_in_blagoApp.swift
+//  Shader-in-blago
+//
+//  Created by Pavel Korostelev on 02.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct Shader_in_blagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shader-in-blagoUITests/Shader_in_blagoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Shader_in_blagoUITestsLaunchTests.swift
+//  Shader-in-blagoUITests
+//
+//  Created by Pavel Korostelev on 02.07.2026.
+//
+
+import XCTest
+
+final class Shader_in_blagoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
